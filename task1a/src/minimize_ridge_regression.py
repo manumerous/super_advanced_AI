e9146bb985f94d691308df5a51709e8b0cc1860f
manuelga <manuelga@student.ethz.ch>
2020-03-13 14:13:53 +0100

    started implementation of minimize ridge regression

--- task1a/src/minimize_ridge_regression.py
+++ task1a/src/minimize_ridge_regression.py
@@ -1,15 +1,17 @@
+import numpy as np
+
+def _calculate_optimization_cost(data, weights, regularization_parameters):
+    cost = 
+
 def minimize_ridge_regression(regression_vector, regularization_parameters):
-    rownumber = len(y)
-    sum = 0
 
-# rsme calculation requires input of two lists (y and y_hat values)
-    for i in range(len(y)):
-        x = (y[i] - y_hat[i])*(y[i] - y_hat[i])
-        sum = sum + x
-    rsme_variable = (1/rownumber*sum) ** 0.5
-    return rsme_variable
+    data_length = len(regression_vector.data)
+    weights = np.ones(data_length)
 
 
-# to test function: run file as main file, otherwise use function in other file
+    return 0
+
+
+# for isolated testing purposes only:
 if __name__ == "main":
     data = [3, 3, 3, 3, 3, 3, 3]
